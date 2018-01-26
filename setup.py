26bef4cb7d94b4f528fd0b975737d94de2563ce6
Roee Babayoff <roeebaba@gmail.com>
2018-01-26 03:10:19 +0200

    Version 1.0!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='maccabistats',
-    version='0.9',
+    version='1.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT',
