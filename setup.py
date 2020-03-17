5e1c5ec0319c11ad226c21dc754bb04ac2d7ec27
kosh-b <roeebaba@gmail.com>
2020-03-18 01:19:10 +0200

    Change install_requires dependencies to be less strict

--- setup.py
+++ setup.py
@@ -11,12 +11,11 @@ setup(
     long_description=open('README.md', encoding="utf-8").read(),
     long_description_content_type='text/markdown',
     python_requires='>=3',
-    install_requires=["setuptools==28.8.0",
-                      "requests==2.20.0",
-                      "beautifulsoup4==4.6.0",
-                      "lxml==4.1.1",
-                      "python-dateutil==2.7.2",
-                      "matplotlib==2.2.2",
-                      "pathlib==1.0.1",
-                      "progressbar==2.5"]
+    install_requires=["setuptools>=28.*",
+                      "requests>=2.20, <3",
+                      "beautifulsoup4>=4.6, <5",
+                      "lxml>=4.1, <5",
+                      "python-dateutil>=2.7, <5",
+                      "matplotlib>=2.2.2, <3",
+                      "progressbar>=2.5, <3"]
 )
