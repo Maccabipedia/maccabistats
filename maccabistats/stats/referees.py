b669258fb77436e3fbc4a00800481ce71e2c0540
Roee Babayoff <roeebaba@gmail.com>
2020-11-06 02:27:03 +0200

    Annotated More

--- maccabistats/stats/referees.py
+++ maccabistats/stats/referees.py
@@ -1,47 +1,38 @@
-# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Tuple, List
+
+if TYPE_CHECKING:
+    from maccabistats.stats.maccabi_games_stats import MaccabiGamesStats
 
 from collections import Counter
 
-
-# This class will handle all referees statistics.
+RefereeStats = Tuple[str, int]  # Referee name to the current stat (an int ranking)
+RefereePercentageStats = Tuple[str, float]  # Referee name to the current stat percentage (float)
 
 
 class MaccabiGamesRefereesStats(object):
+    """
+    This class will handle all referees statistics.
+    """
 
-    def __init__(self, maccabi_games_stats):
-        """
-        :type maccabi_games_stats: maccabistats.stats.maccabi_games_stats.MaccabiGamesStats
-        """
-
+    def __init__(self, maccabi_games_stats: MaccabiGamesStats) -> None:
         self.games = maccabi_games_stats.games
 
     @property
-    def most_judged_referee(self):
-        """
-        :rtype: Counter
-        """
+    def most_judged_referee(self) -> List[RefereeStats]:
         return Counter(game.referee for game in self.games).most_common()
 
     @property
-    def best_referee(self):
-        """
-        :rtype: Counter
-        """
+    def best_referee(self) -> List[RefereeStats]:
         return Counter(game.referee for game in self.games if game.is_maccabi_win).most_common()
 
     @property
-    def worst_referee(self):
-        """
-        :rtype: Counter
-        """
+    def worst_referee(self) -> List[RefereeStats]:
         return Counter(game.referee for game in self.games if game.maccabi_score_diff < 0).most_common()
 
     @property
-    def best_referee_by_percentage(self):
-        """
-        :rtype: Counter
-        """
-
+    def best_referee_by_percentage(self) -> List[RefereePercentageStats]:
         # Both return as Counter.most_common() which is list (of tuples)
         judged_games = Counter(dict(self.most_judged_referee))
         games_won_with_referees = Counter(dict(self.best_referee))
@@ -54,11 +45,7 @@ class MaccabiGamesRefereesStats(object):
         return best_referee.most_common()
 
     @property
-    def worst_referee_by_percentage(self):
-        """
-        :rtype: Counter
-        """
-
+    def worst_referee_by_percentage(self) -> List[RefereePercentageStats]:
         # Both return as Counter.most_common() which is list (of tuples)
         judged_games = Counter(dict(self.most_judged_referee))
         games_lost_with_referees = Counter(dict(self.worst_referee))
